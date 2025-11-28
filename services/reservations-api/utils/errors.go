5dd4bd93f8d5b035967b72696be5782a7a4f35f1
Ismael Cuello <2306347@UCC.edu.ar>
2025-11-28 11:16:49 -0300

    filtros solr

--- services/reservations-api/utils/errors.go
+++ services/reservations-api/utils/errors.go
@@ -8,4 +8,5 @@ var (
 	ErrReservationNotFound    = errors.New("reservation not found")
 	ErrInvalidReservationData = errors.New("invalid reservation data")
 	ErrInternalServer         = errors.New("internal server error")
+	ErrReservationConflict    = errors.New("room already reserved for selected dates")
 )
