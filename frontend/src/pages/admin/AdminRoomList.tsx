00b5a991d973d1e34b639f7f8a7359209e7e537e
Ismael Cuello <2306347@UCC.edu.ar>
2025-11-28 12:11:44 -0300

    arreglo puerto

--- frontend/src/pages/admin/AdminRoomList.tsx
+++ frontend/src/pages/admin/AdminRoomList.tsx
@@ -250,7 +250,7 @@ export default function AdminRoomList() {
                                 name="maxPrice"
                                 value={filters.maxPrice}
                                 onChange={handleFilterChange}
-                                placeholder="Any Price"
+                                placeholder="Cualquier precio"
                                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                             />
                         </div>
